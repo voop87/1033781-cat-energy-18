b465a3943d7bff66c7849ee0a356f6d1a21da00e
Ruslan Mansurov <mansurov87@gmail.com>
2019-12-16 00:57:58 +0300

    Добавляет начальную стилизацию слайдера и карт (mobile, catalog)

--- source/js/app.js
+++ source/js/app.js
@@ -1,19 +1,12 @@
-var slideBefore = document.getElementById("slider__image--before");
-var slideAfter = document.getElementById("slider__image--after");
-var btnBefore = document.getElementById("slider__control--before");
-var btnAfter = document.getElementById("slider__control--after");
-var progress = document.getElementById("slider__progress");
+//Интерактивная карта Google
+function initMap() {
+  var address = {lat: 59.9386667, lng: 30.321771};
+  var map = new google.maps.Map(
+      document.querySelector('.page-footer__map'), {zoom: 16, center: address});
 
-var showSlideBefore = function () {
-  slideAfter.setAttribute("hidden", "hidden");
-  slideBefore.removeAttribute("hidden");
-  progress.style.left = "5px";
-};
-var showSlideAfter = function () {
-  slideBefore.setAttribute("hidden", "hidden");
-  slideAfter.removeAttribute("hidden");
-  progress.style.left = "42px";
-};
-
-btnBefore.addEventListener("click", showSlideBefore);
-btnAfter.addEventListener("click", showSlideAfter);
+  var marker = new google.maps.Marker({
+    position: address,
+    map: map,
+    icon: '../img/map-pin-google.png'
+  });
+}
--- source/js/slider.js
+++ source/js/slider.js
@@ -1,19 +1,13 @@
-var slideBefore = document.getElementById("slider__image--before");
-var slideAfter = document.getElementById("slider__image--after");
-var btnBefore = document.getElementById("slider__control--before");
-var btnAfter = document.getElementById("slider__control--after");
-var progress = document.getElementById("slider__progress");
+var slideBefore = document.querySelector('.slider__image--before');
+var sliderProgress = document.querySelector('.slider__progress');
 
-var showSlideBefore = function () {
-  slideAfter.setAttribute("hidden", "hidden");
-  slideBefore.removeAttribute("hidden");
-  progress.style.left = "5px";
-};
-var showSlideAfter = function () {
-  slideBefore.setAttribute("hidden", "hidden");
-  slideAfter.removeAttribute("hidden");
-  progress.style.left = "42px";
-};
+function showSlide(widthPercent, progress) {
+  slideBefore.style.width = widthPercent;
 
-btnBefore.addEventListener("click", showSlideBefore);
-btnAfter.addEventListener("click", showSlideAfter);
+  if (progress === '--left') {
+    sliderProgress.classList.remove('slider__progress--right');
+  } else {
+    sliderProgress.classList.remove('slider__progress--left');
+  }
+  sliderProgress.classList.add('slider__progress' + progress);
+}
