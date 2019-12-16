dac4e1be9147f6571b7b43419019dc648efe9873
Ruslan Mansurov <mansurov87@gmail.com>
2019-12-16 17:51:38 +0300

    Добавляет скрипты выпадающего меню и подключает интерактивную карту на все страницы

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,17 @@
+//Выпадающее меню
+function showMenu() {
+  var menuBtn = document.querySelector('.page-header__menu-toggle');
+  var menu = document.querySelector('.main-nav');
+
+  menuBtn.classList.toggle('page-header__menu-toggle--closed');
+  menu.classList.toggle('main-nav--closed');
+}
+
 //Интерактивная карта Google
 function initMap() {
   var address = {lat: 59.9387412, lng: 30.3228941};
   var map = new google.maps.Map(
-      document.querySelector('.page-footer__map'), {zoom: 18, center: address});
+      document.querySelector('.page-footer__google-map'), {zoom: 18, center: address});
 
   var marker = new google.maps.Marker({
     position: address,
