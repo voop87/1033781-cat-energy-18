7f619eca298e0b2177bd85de5a8fe42f600b2909
Ruslan Mansurov <mansurov87@gmail.com>
2019-12-16 15:20:56 +0300

    Завершает стилизацию индекс (catalog, desktop)

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,8 @@
 //Интерактивная карта Google
 function initMap() {
-  var address = {lat: 59.9386667, lng: 30.321771};
+  var address = {lat: 59.9387412, lng: 30.3228941};
   var map = new google.maps.Map(
-      document.querySelector('.page-footer__map'), {zoom: 16, center: address});
+      document.querySelector('.page-footer__map'), {zoom: 18, center: address});
 
   var marker = new google.maps.Marker({
     position: address,
