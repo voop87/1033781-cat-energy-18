b615587d45951f6bca1da126372db31c05d28ecc
Ruslan Mansurov <mansurov87@gmail.com>
2019-12-27 16:38:31 +0300

    Исправляет замечания Б28, Б30

--- source/js/app.js
+++ source/js/app.js
@@ -1,21 +1,30 @@
 //Выпадающее меню
-function showMenu() {
-  var menuBtn = document.querySelector('.page-header__menu-toggle');
-  var menu = document.querySelector('.main-nav');
+var menuBtn = document.querySelector(".page-header__menu-toggle");
+var menu = document.querySelector('.main-nav');
 
-  menuBtn.classList.toggle('page-header__menu-toggle--closed');
-  menu.classList.toggle('main-nav--closed');
+window.addEventListener("resize", function() {
+  if (window.matchMedia("(min-width: 738px)").matches) {
+    menuBtn.style.display = "none";
+    console.log(menuBtn.style.display);
+  } else {
+    menuBtn.style.display = "block";
+  }
+});
+
+function showMenu() {
+  menuBtn.classList.toggle("page-header__menu-toggle--closed");
+  menu.classList.toggle("main-nav--closed");
 }
 
 //Интерактивная карта Google
 function initMap() {
   var address = {lat: 59.9387412, lng: 30.3228941};
   var map = new google.maps.Map(
-      document.querySelector('.page-footer__google-map'), {zoom: 18, center: address});
+      document.querySelector(".page-footer__google-map"), {zoom: 18, center: address});
 
   var marker = new google.maps.Marker({
     position: address,
     map: map,
-    icon: '../img/map-pin-google.png'
+    icon: "../img/map-pin-google.png"
   });
 }
